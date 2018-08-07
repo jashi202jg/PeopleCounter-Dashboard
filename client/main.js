7114fa95bec529e278d9c54bbab1a05fab5b375b
JASHIM GAFOOR K K <jashi202jg@gmail.com>
2018-08-07 23:53:12 +0530

    responsive

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import dashBoard from '../imports/components/dashBoard/dashBoard';
-
  
 
 angular.module('people-counter', [
